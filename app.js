9a27dcded55616757d8aecf085c881029b27f335
deepak-singh-jethi <deepujethi@yahoo.com>
2024-03-12 15:04:31 +0530

    express basics

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const express = require("express");
+const { log } = require("console");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Inside the middleWare");
+  next(); // Allows the request to continue to the next middleware in line.
+});
+
+app.use((req, res, next) => {
+  console.log("inside another middleware");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
