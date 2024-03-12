3c0afd05e83fe06b0172965f280978fc1f7ac10f
deepak-singh-jethi <deepujethi@yahoo.com>
2024-03-12 15:15:41 +0530

    express basics , use of middleware

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use((req, res, next) => {
   res.send("<h1>Hello from Express</h1>");
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// server.listen(3000);
+
+app.listen(3000);
