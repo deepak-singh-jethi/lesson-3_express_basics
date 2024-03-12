3c797fef7efe074ca0447f49108628684c949871
deepak-singh-jethi <deepujethi@yahoo.com>
2024-03-12 22:28:27 +0530

    useing helper function to locate rootDir

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const http = require("http");
+// const http = require("http");
+
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+const rootDir = require("./helper/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(shopRoutes);
 app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 //catch routes error if no page with given route is found then show this page
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "noPageFound.html"));
 });
 
 app.listen(3000);
