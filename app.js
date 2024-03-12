bc80969c27672e2b5e8af95bbea8cc4be2138749
deepak-singh-jethi <deepujethi@yahoo.com>
2024-03-12 18:34:58 +0530

    learn adding routes, exporting them, using prefix in routes using express

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const http = require("http");
 const express = require("express");
-const { log } = require("console");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+
+//catch routes error if no page with given route is found then show this page
 app.use((req, res, next) => {
-  console.log("Inside the middleWare");
-  next(); // Allows the request to continue to the next middleware in line.
+  res.status(404).send("<h1>Page not found</h1>");
 });
-
-app.use((req, res, next) => {
-  console.log("inside another middleware");
-  res.send("<h1>Hello from Express</h1>");
-});
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
 
 app.listen(3000);
